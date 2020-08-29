a4076468260b5f7ef5078aab87f73be3fff859d7
vsidera <siderravictor@gmail.com>
2020-08-29 12:28:42 +0300

    add the github url to the service

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData():Observable<any>{
+
+     const url = "https://api.github.com/users"
+
+     return this.http.get<any>(url)
+  }
 }
