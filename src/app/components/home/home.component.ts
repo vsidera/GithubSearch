ae13787d81fcd57c5ac7ccaa2b757decac0020e8
vsidera <siderravictor@gmail.com>
2020-08-29 12:17:31 +0300

    add the getUsers method

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  users: string[];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  getUsers(){
+    
   }
 
 }
