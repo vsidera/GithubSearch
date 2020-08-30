5ae7ccefaac7bc79ec84d6e2a58ea30fbd3fb233
vsidera <siderravictor@gmail.com>
2020-08-30 23:17:57 +0300

    add bootstrap

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Observable} from 'rxjs';
 import { UserModel } from 'src/app/Models/user';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class GithubService {
 
-  private url:string = 'https://api.github.com/users'
+  url:string = "https://api.github.com/users"
   usersLimit = '?_limit=5'
 
   constructor(private http: HttpClient) { }
